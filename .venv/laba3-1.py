817cbe6f232fc987a0d0f3adfaad55b7242abfbf
Tymofii Ivzhenko <tymofiiivzhenko@gmail.com>
2024-09-04 14:10:44 +0100

    laba 3

--- .venv/laba3-1.py
+++ .venv/laba3-1.py
@@ -3,6 +3,6 @@ word = str(input("Введіть слово: "))
 while len(word) < 3:
     word = str(input("Слово занадто коротке. Введіть слово ще раз: "))
 
-new_word = word[:1] + word[2:-2] + word[-1]
+new_word = word[1] + word[-2]
 
 print("Після зрізу другого та передостаннього символів: ", new_word)
