b7e5abbce2375e738eb1536a99cdd717b49fdbb6
Tymofii Ivzhenko <tymofiiivzhenko@gmail.com>
2024-09-18 11:48:46 +0100

    laba 3

---
+++ .venv/laba5-1.py
@@ -0,0 +1,16 @@
+n = int(input("n = "))
+print(f"Введіть {n} елементів масиву:")
+arr = [float(input()) for _ in range(n)]
+print("Одновимірний масив:", arr)
+
+max_negative = None
+
+for num in arr:
+    if num < 0:
+        if max_negative is None or num < max_negative:
+            max_negative = num
+
+if max_negative is not None:
+    print(f"Максимальний від'ємний елемент: {max_negative}")
+else:
+    print("У масиві немає від'ємних елементів.")
