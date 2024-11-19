ed84b9dda1a9e5bf4abbe2db38430a714e1248e5
Tymofii Ivzhenko <tymofiiivzhenko@gmail.com>
2024-11-19 22:52:49 +0000

    laba 14

--- .venv/laba14-1.py
+++ .venv/laba14-1.py
@@ -1,15 +1,15 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-x = np.linspace(0.1, 10, 1000)
+x = np.linspace(-3, 3, 1000)
 
-y = np.cos(x**2) / x
+y = 15 * np.sin(10 * x) * np.cos(3 * x)
 
 plt.figure(figsize=(8, 6))
-plt.plot(x, y, label=r"$Y(x) = \frac{\cos(x^2)}{x}$", color='blue')
+plt.plot(x, y, label=r"$Y(x) = 15 \sin(10x) \cos(3x)$", color='blue')
 plt.axhline(0, color='black', linewidth=0.5, linestyle='--')  # Лінія y = 0
 plt.axvline(0, color='black', linewidth=0.5, linestyle='--')  # Лінія x = 0
-plt.title("Графік функції $Y(x) = \\frac{\\cos(x^2)}{x}$")
+plt.title("Графік функції $Y(x) = 15*sin(10*x)*cos(3*x)$")
 plt.xlabel("x")
 plt.ylabel("Y(x)")
 plt.legend()
